01d57321683cb00194436b2ee3f3f1dd53c98ee7
Thomas Dost <stu203329@mail.uni-kiel.de>
2023-09-21 11:19:36 +0200

    Fixed and finalized all first level analysis - Sig. reactions - Sig. macs

--- code/mag_abundance_analysis.R
+++ code/mag_abundance_analysis.R
@@ -28,6 +28,7 @@ calculate_p_values <- function(counts1,counts2, counts3,age = 2)
   counts3 = as.data.frame(counts3)
   commons = intersect(rownames(counts1), rownames(counts2))
   commons = intersect(commons, rownames(counts3))
+  # Only using reactions that are present in all three sites
   print(age)
   print(length(commons))
   models =  list()
@@ -42,15 +43,27 @@ calculate_p_values <- function(counts1,counts2, counts3,age = 2)
     current_otu2 = merge(current_otu, d2   , by.x = 0, by.y = 0 )
     current_otu3 = merge(current_otu2, d3  , by.x = "Row.names", by.y = 0 )
     colnames(current_otu3) <- c("patID", "cecum_count", "colon_count", "stool_count")
-    rownames(current_otu3) <- gsub(".*/", "", current_otu3$patID)
+    #rownames(current_otu3) <- gsub(".*/", "", current_otu3$patID)
     temp_meta = metadata %>% select(c(Age, ID))
     temp_meta$ID =  gsub("_.", "", temp_meta$ID)
     temp_meta = unique(temp_meta)
-    current_otu4 = merge(current_otu3, temp_meta,  by.x = 0, by.y = "ID")
+    temp_meta$fullID = apply(temp_meta, 1, function (row) { str_trim(paste0(row[[1]], ".", row[[2]]))})
+    temp_meta$fullID = as.character(temp_meta$fullID)
+    current_otu3$patID = as.character(current_otu3$patID)
+    current_otu3$patID
+    temp_meta$fullID
+    intersect(current_otu3$patID, temp_meta$fullID)
+    # MAGS USE DIFFERENT IDS
+    current_otu3$patID   = gsub("Sum_", "", current_otu3$patID )
+    current_otu3$patID   = gsub("/", ".", current_otu3$patID )
+    current_otu4 = merge(current_otu3, temp_meta,  by.x = "patID", by.y = "fullID")
+    if(nrow(current_otu4) != nrow(current_otu)) {
+      stop()
+    }
     current_otu4 = current_otu4[current_otu4$Age == age, ]
     rownames(current_otu4) = current_otu4$Row.names
     current_otu4 = current_otu4[-c(1)]
-    colnames(current_otu4) <- c("ID", "1_","2_","3_","Age")
+    colnames(current_otu4) <- c("1_","2_","3_","Age", "ID")
     long_data <- pivot_longer(
       data = current_otu4,
       cols = ends_with("_"),
@@ -88,16 +101,21 @@ calculate_p_values <- function(counts1,counts2, counts3,age = 2)
 # Load 16S to MAG mapping
 # 161
 cecumMAGCounts <- cecum_counts
-
+cecumMAGCounts = cecumMAGCounts %>% column_to_rownames("V2")
 # 161
 colonMAGCounts <- colon_counts
+colonMAGCounts = colonMAGCounts %>% column_to_rownames("V2")
 
 # 161
 stoolMAGCounts <- stool_counts
+stoolMAGCounts = stoolMAGCounts %>% column_to_rownames("V2")
 
 cecumMAGCounts = cecumMAGCounts[-nearZeroVar(t(cecumMAGCounts)), ]
+# 150
 colonMAGCounts = colonMAGCounts[-nearZeroVar(t(colonMAGCounts)), ]
+# 154
 stoolMAGCounts = stoolMAGCounts[-nearZeroVar(t(stoolMAGCounts)), ]
+# 154
 
 ### Check if we have the same MAGs in all three communities
 all.equal(rownames(stoolMAGCounts), rownames(colonMAGCounts))
@@ -107,11 +125,11 @@ write.csv(cecumMAGCounts, file = "../data/filtered_relative_cecum_mag_counts.csv
 write.csv(colonMAGCounts, file = "../data/filtered_relative_colon_mag_counts.csv")
 write.csv(stoolMAGCounts, file = "../data/filtered_relative_stool_mag_counts.csv")
 
-Two_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stool_counts)
-Nine_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stool_counts, age = "9")
-Fifteen_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stool_counts, age = "15")
-TwentyFour_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stool_counts, age = "24")
-Thirty_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stool_counts, age = "30")
+Two_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stoolMAGCounts)
+Nine_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stoolMAGCounts, age = "9")
+Fifteen_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stoolMAGCounts, age = "15")
+TwentyFour_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stoolMAGCounts, age = "24")
+Thirty_month_MAG <- calculate_p_values(cecumMAGCounts, colonMAGCounts, stoolMAGCounts, age = "30")
 
 
 write.csv(Two_month_MAG, "../data/mag_correlation_2M.csv")
@@ -120,15 +138,15 @@ write.csv(Fifteen_month_MAG, "../data/mag_correlation_15M.csv")
 write.csv(TwentyFour_month_MAG, "../data/mag_correlation_24M.csv")
 write.csv(Thirty_month_MAG, "../data/mag_correlation_30M.csv")
 
-cecumRxNCounts = as.matrix(read.csv("../data/cecum_rxn_abundance.csv", check.names = F, row.names = 1))
+cecumRxNCounts = as.matrix(read.csv("../data/relative_cecum_rxn_abundance.csv", check.names = F, row.names = 1))
 cecumRxNCounts <- apply(cecumRxNCounts, 2, function(col) {col/sum(col)})
 cecumRxNCounts = cecumRxNCounts[-nearZeroVar(t(cecumRxNCounts)), ]
 
-colonRxNCounts = as.matrix(read.csv("../data/colon_rxn_abundance.csv", check.names = F, row.names = 1))
+colonRxNCounts = as.matrix(read.csv("../data/relative_colon_rxn_abundance.csv", check.names = F, row.names = 1))
 colonRxNCounts <- apply(colonRxNCounts, 2, function(col) {col/sum(col)})
 colonRxNCounts = colonRxNCounts[-nearZeroVar(t(colonRxNCounts)), ]
 
-stoolRxNCounts = as.matrix(read.csv("../data/stool_rxn_abundance.csv", check.names = F, row.names = 1))
+stoolRxNCounts = as.matrix(read.csv("../data/relative_stool_rxn_abundance.csv", check.names = F, row.names = 1))
 stoolRxNCounts <- apply(stoolRxNCounts, 2, function(col) {col/sum(col)})
 stoolRxNCounts = stoolRxNCounts[-nearZeroVar(t(stoolRxNCounts)), ]
 
