6a618b4daf5bcb9cc54e853beb6edb4c7c283037
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-01-12 10:12:48 +0100

    Added new merged HQ+MQ models

---
+++ code/communities/prepare_com.R
@@ -0,0 +1,27 @@
+setwd("/home/td/Projects/Aging/Baseline/GIN/code")
+source("utils.R")
+# Unnnormalized, unfiltered (by HQness) ABSOLUTE MAG counts
+cecum_counts_inc_mq  <-  filter_per_organ(metadata, count_data, "Cecum", filter_HQ_only = FALSE, normalize = FALSE)
+colon_counts_inc_mq  <-  filter_per_organ(metadata, count_data, "Colon", filter_HQ_only = FALSE, normalize = FALSE)
+stool_counts_inc_mq  <-  filter_per_organ(metadata, count_data, "stool", filter_HQ_only = FALSE, normalize = FALSE)
+
+
+# We dont not have all samples for all three organs thus we take the intersection
+samples_in_all_three <- intersect(colnames(cecum_counts_inc_mq), colnames(colon_counts_inc_mq))
+samples_in_all_three <- intersect(samples_in_all_three, colnames(stool_counts_inc_mq))
+cecum_counts_inc_mq_intersected <- cecum_counts_inc_mq[samples_in_all_three]
+colon_counts_inc_mq_intersected <- colon_counts_inc_mq[samples_in_all_three]
+stool_counts_inc_mq_intersected <- stool_counts_inc_mq[samples_in_all_three]
+
+colnames(cecum_counts_inc_mq_intersected) <- gsub("Sum_", "",   colnames(cecum_counts_inc_mq_intersected))
+colnames(colon_counts_inc_mq_intersected) <- gsub("Sum_", "",   colnames(colon_counts_inc_mq_intersected))
+colnames(stool_counts_inc_mq_intersected) <- gsub("Sum_", "",   colnames(stool_counts_inc_mq_intersected))
+
+nrow(stool_counts_inc_mq_intersected) == 161
+nrow(colon_counts_inc_mq_intersected) == 161
+nrow(cecum_counts_inc_mq_intersected) == 161
+
+
+write.csv(cecum_counts_inc_mq_intersected, file = "cecum_counts_inc_mq_intersected.tsv")
+write.csv(colon_counts_inc_mq_intersected, file = "colon_counts_inc_mq_intersected.tsv")
+write.csv(stool_counts_inc_mq_intersected, file = "stool_counts_inc_mq_intersected.tsv")
